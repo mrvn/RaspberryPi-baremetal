b79bd8c91ffa292010475a8bf4d071e8adb5b6bf
Goswin von Brederlow <goswin-v-b@web.de>
2015-03-07 13:29:45 +0100

    typo fix

--- 003-can-you-c-me/arch_info.h
+++ 003-can-you-c-me/arch_info.h
@@ -15,7 +15,7 @@
    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 */
 
-/* Information about the differe Raspberry Pi models
+/* Information about the different Raspberry Pi models
  */
 
 #ifndef KERNEL_ARCHINFO_H
